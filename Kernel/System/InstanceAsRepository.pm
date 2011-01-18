2ba192dacdbd42a4346719831a2ec10506af3a61
reneeb <github@renee-baecker.de>
2011-01-18 15:47:38 +0100

    use own table and move a method from package class to this one

--- Kernel/System/InstanceAsRepository.pm
+++ Kernel/System/InstanceAsRepository.pm
@@ -122,7 +122,7 @@ sub PackageIsApproved {
     }
 
     return if !$Self->{DBObject}->Prepare(
-        SQL  => 'SELECT approved FROM package_repository '
+        SQL  => 'SELECT approved FROM instance_package_repository '
             . 'WHERE name = ? AND version = ? AND approved = version',
         Bind => [ \$Param{Name}, \$Param{Version} ],
     );
@@ -146,11 +146,13 @@ returns a list of repository packages
 sub RepositoryList {
     my ( $Self, %Param ) = @_;
 
-    $Self->{DBObject}->Prepare(
-        SQL => 'SELECT id, name, version, approved, content '
-            . 'FROM package_repository ORDER BY name, create_time',
+    return if !$Self->{DBObject}->Prepare(
+        SQL => 'SELECT pr.id, pr.name, pr.version, pr.approved, pr.content '
+            . 'FROM instance_package_repository pr '
+            . 'ORDER BY pr.name, pr.create_time',
     );
 
+    my %PackagesSeen;
     my @Data;
     while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
         my $IsApproved = ( $Row[3] && $Row[3] eq $Row[2] ) ? $Row[3] : 0;
@@ -162,7 +164,38 @@ sub RepositoryList {
             Content   => { Content => $Row[4] },
         );
 
+        $PackagesSeen{ $Row[1] . '-' . $Row[2] }++;
+
         push @Data, \%Info;
+    }
+
+    return if !$Self->{DBObject}->Prepare(
+        SQL => 'SELECT pr.id, pr.name, pr.version, pr.content '
+            . 'FROM package_repository pr '
+            . 'ORDER BY pr.name, pr.create_time',
+    );
+
+    while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
+        next if $PackagesSeen{ $Row[1] . '-' . $Row[2] };
+        my %Info = (
+            PackageID => $Row[0],
+            Name      => { Content => $Row[1] },
+            Version   => { Content => $Row[2] },
+            Approved  => { Content => 0 },
+            Content   => { Content => $Row[4] },
+        );
+
+        push @Data, \%Info;
+    }
+
+    my %MaxVersions;
+
+    for my $Key ( keys %PackagesSeen ) {
+        my ($Name,$Version) = split /-/, $Key;
+        my $Numified        = sprintf "%03d%04d%04d", split /\./, $Version;
+        if ( !$MaxVersions{$Name} || $MaxVersions{$Name} < $Numified ) {
+            $MaxVersions{$Name} = $Numified;
+        }
     }
 
     for my $Entity (@Data) {
@@ -173,6 +206,11 @@ sub RepositoryList {
             my %Structure = $Self->{PackageObject}->PackageParse(
                 String => \$Entity->{Content}->{Content},
             );
+
+            my $Numified = sprintf "%03d%04d%04d", split /\./, $Structure{Version}->{Content};
+            if ( $Param{Distinct} && $Numified != $MaxVersions{ $Entity->{Name}->{Content} } ) {
+                $Approved = 0;
+            }
 
             $Entity = {
                 %Structure,
@@ -191,6 +229,7 @@ approve a package from local repository
 
     my $InstanceAsRepository = $InstanceAsRepositoryObject->PackageApprove(
         PackageID => 123,
+        UserID    => 10,
     );
 
 =cut
@@ -199,7 +238,7 @@ sub PackageApprove {
     my ( $Self, %Param ) = @_;
 
     # check needed stuff
-    for my $Needed (qw(PackageID)) {
+    for my $Needed (qw(PackageID UserID)) {
         if ( !$Param{$Needed} ) {
             $Self->{LogObject}->Log(
                 Priority => 'error',
@@ -209,10 +248,42 @@ sub PackageApprove {
         }
     }
 
+    return if !$Self->{DBObject}->Prepare(
+        SQL   => 'SELECT name, version, content FROM package_repository WHERE id = ?',
+        Bind  => [ \$Param{PackageID} ],
+        Limit => 1,
+    );
+
+    my %Info;
+    while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
+        %Info = (
+            Name    => $Row[0],
+            Version => $Row[1],
+            Content => $Row[2],
+        );
+    }
+
+    return if !%Info;
+
+    return if $Self->PackageIsApproved(
+        Name    => $Info{Name},
+        Version => $Info{Version},
+    );
+
     # db access
     return $Self->{DBObject}->Do(
-        SQL => 'UPDATE package_repository SET approved = version WHERE id = ?',
-        Bind => [ \$Param{PackageID} ],
+        SQL => 'INSERT INTO instance_package_repository ( approved, name, version, content, '
+            . 'package_id, create_time, create_by, change_time, change_by ) '
+            . 'VALUES (?,?,?,?,?,current_timestamp,?,current_timestamp,?)',
+        Bind => [
+            \$Info{Version},
+            \$Info{Name},
+            \$Info{Version},
+            \$Info{Content},
+            \$Param{PackageID},
+            \$Param{UserID},
+            \$Param{UserID},
+        ],
     );
 }
 
@@ -242,9 +313,64 @@ sub PackageRevoke {
 
     # db access
     return $Self->{DBObject}->Do(
-        SQL => 'UPDATE package_repository SET approved = NULL WHERE id = ?',
+        SQL => 'DELETE FROM instance_package_repository WHERE id = ?',
         Bind => [ \$Param{PackageID} ],
     );
+}
+
+=item RepositoryGet()
+
+get a package from local repository
+
+    my $Package = $PackageObject->RepositoryGet(
+        Name    => 'Application A',
+        Version => '1.0',
+    );
+
+    my $PackageScalar = $PackageObject->RepositoryGet(
+        Name    => 'Application A',
+        Version => '1.0',
+        Result  => 'SCALAR',
+    );
+
+=cut
+
+sub RepositoryGet {
+    my ( $Self, %Param ) = @_;
+
+    # check needed stuff
+    for (qw(Name Version)) {
+        if ( !defined $Param{$_} ) {
+            $Self->{LogObject}->Log(
+                Priority => 'error',
+                Message => "$_ not defined!",
+            );
+            return;
+        }
+    }
+
+    # db access
+    $Self->{DBObject}->Prepare(
+        SQL => 'SELECT content FROM instance_package_repository WHERE name = ? AND version = ?',
+        Bind => [ \$Param{Name}, \$Param{Version} ],
+    );
+    my $Package = '';
+    while ( my @Row = $Self->{DBObject}->FetchrowArray() ) {
+        $Package = $Row[0];
+    }
+    if ( !$Package ) {
+        $Self->{LogObject}->Log(
+            Priority => 'notice',
+            Message  => "No such package $Param{Name}-$Param{Version}!",
+        );
+        return;
+    }
+
+    if ( $Param{Result} && $Param{Result} eq 'SCALAR' ) {
+        return \$Package;
+    }
+
+    return $Package;
 }
 
 1;
